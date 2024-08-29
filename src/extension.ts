475df52e6b060cf42ad8fe8e649a192c1a8eb5f5
Jonah Iden <jonah.iden@typefox.io>
2024-08-29 12:06:39 +0200

    added vscode execute commands
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- src/extension.ts
+++ src/extension.ts
@@ -10,16 +10,28 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('notebook-selection-test.get-current-selection', () => {
+	context.subscriptions.push(
+	vscode.commands.registerCommand('notebook-selection-test.get-current-selection', () => {
 		if(vscode.window.activeNotebookEditor) {
 			const notebookEditor = vscode.window.activeNotebookEditor;
 			vscode.window.showInformationMessage(`current notebook: ${notebookEditor.notebook.uri.toString()} with selection ${notebookEditor.selection?.start}`);
 		} else {
 			vscode.window.showInformationMessage('No active notebook editor');
 		}
-	});
+	}),
+	vscode.commands.registerCommand('notebook-selection-test.executeFoldingRangeProvider', async () => {
+		const res = await vscode.commands.executeCommand('vscode.executeFoldingRangeProvider', vscode.window.activeTextEditor?.document.uri);
+		vscode.window.showInformationMessage(`Folding ranges: ${JSON.stringify(res)}`);
+	}),
+	vscode.commands.registerCommand('notebook-selection-test.executeCodeActionProvider', async () => {
+		const res = await vscode.commands.executeCommand('vscode.executeCodeActionProvider', vscode.window.activeTextEditor?.document.uri, vscode.window.activeTextEditor?.selection);
+		vscode.window.showInformationMessage(`Code Actions: ${JSON.stringify(res)}`);
+	}),
+	vscode.commands.registerCommand('notebook-selection-test.executeWorkspaceSymbolProvider', async () => {
+		const res = await vscode.commands.executeCommand('vscode.executeWorkspaceSymbolProvider', 'test');
+		vscode.window.showInformationMessage(`Workspace Symbols: ${JSON.stringify(res)}`);
+	}));
 
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
